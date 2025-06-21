dcfcff1528f031bfe2aab6a64572ab9b237dd9a7
Vishal Gupta <vishalgupta252310@gmail.com>
2025-06-21 22:30:06 +0530

    added connection with database

--- src/app.js
+++ src/app.js
@@ -1,23 +1,13 @@
 const express = require("express");
-
 const app = express();
+const makeConnectionWithDB = require("./config/database");
 
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("Something went wrong");
-  }
-
-  next();
-});
-app.get("/getUserData", (req, res) => {
-  try {
-    throw new Error();
-    res.send("Data fetching successfully");
-  } catch (error) {
-    res.send("something went wrong please contact developer");
-  }
-});
-
-app.listen("7777", () => {
-  console.log("Server is listening on port 7777.");
-});
+makeConnectionWithDB()
+  .then(() => {
+    app.listen("7777", () => {
+      console.log("Server is listening on port 7777.");
+    });
+  })
+  .catch(() => {
+    console.error("something went wrong");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const mongodbUri =
+  "mongodb+srv://vishalgupta252310:vishalgupta252310@cluster0.nedg5.mongodb.net/Leap";
+
+const makeConnectionWithDB = async (err, req, res) => {
+  if (err) {
+    console.log("something went wrong while connecting to db");
+    return;
+  }
+
+  try {
+    await mongoose.connect(mongodbUri);
+    console.log("Database connected successfully.");
+  } catch (error) {
+    console.error("Database connection fails.");
+  }
+};
+
+module.exports = makeConnectionWithDB;
