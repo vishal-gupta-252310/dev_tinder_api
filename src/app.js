f0a22fde571589e463090dc23068b68430d811f4
Vishal Gupta <vishalgupta252310@gmail.com>
2025-06-21 13:55:59 +0530

    checking git account

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 
 app.use("/hello", (req, res) => {
-    res.send("Hi server is saying hello to you on this endpoint.")
-})
+  res.send("Hi server is saying hello to you on this endpoint.");
+});
 
 app.use((req, res) => {
-    res.send("Server is up and running on 7777.")
-})
+  res.send("Server is up and running on 7777.");
+});
 
 app.listen("7777", () => {
-    console.log("Server is listening on port 7777")
+  console.log("Server is listening on port 7777.");
 });
