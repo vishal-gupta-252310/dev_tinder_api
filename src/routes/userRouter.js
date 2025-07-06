45fa4deccc3a9508f0bda98f478d5c8dad48cb58
Vishal Gupta <vishalgupta252310@gmail.com>
2025-07-06 13:15:42 +0530

    created feed api

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,6 +3,7 @@ const { isUserAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const sendResponse = require("../utils/sendResponse");
 const AppError = require("../utils/AppError");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 const USER_SAFE_PUBLIC_DATA =
@@ -58,6 +59,7 @@ userRouter.get("/users/me/connections", isUserAuth, async (req, res) => {
     })
       .populate("fromUserId", USER_SAFE_PUBLIC_DATA)
       .populate("toUserId", USER_SAFE_PUBLIC_DATA);
+
     const filteredUsers = connections.map((row) => {
       if (row?.fromUserId._id.equals(loggedUser?._id)) {
         return row.toUserId;
@@ -73,6 +75,50 @@ userRouter.get("/users/me/connections", isUserAuth, async (req, res) => {
     });
   } catch (error) {
     throw new AppError(400, "Something went wrong.");
+  }
+});
+
+// to get the feed list of logged user
+userRouter.get("/users/me/feed", isUserAuth, async (req, res) => {
+  // to get logged user
+  // get the user listing where
+  // connected user not come
+  // ignored, interested or rejected user should not come
+  // self data should also not come
+
+  // first get users whom the logged user sent request or get request from them and check status also
+  // to get users ids from the hideUsersList and unique also
+  // get all users after excluding the hideUsersFeed user ids and self
+  try {
+    const loggedUser = req.user;
+    const requests = await ConnectionRequest.find({
+      $and: [
+        { status: { $in: ["interested", "ignored", "accepted", "rejected"] } },
+        { $or: [{ fromUserId: loggedUser._id }, { toUserId: loggedUser._id }] },
+      ],
+    });
+
+    const hideUsersFeed = new Set();
+
+    requests.forEach((item) => {
+      hideUsersFeed.add(item.fromUserId.toString());
+      hideUsersFeed.add(item.toUserId.toString());
+    });
+
+    const loggedUserFeedList = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFeed) } },
+        { _id: { $ne: loggedUser._id } },
+      ],
+    }).select(USER_SAFE_PUBLIC_DATA);
+
+    res.json({
+      status: "success",
+      data: loggedUserFeedList,
+      message: "Feed fetched successfully.",
+    });
+  } catch (error) {
+    throw new AppError(400, error.message);
   }
 });
 
